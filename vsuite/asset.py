98be3645ca3213ec4537424582e328e6718ebaa6
Jesse Bulson-Lewis <bulsonjg@whitman.edu>
2018-02-17 14:54:52 -0800

    Add methods to list available files

--- vsuite/asset.py
+++ vsuite/asset.py
@@ -39,7 +39,36 @@ class Asset():
 
         Returns:
             str: relative path to asset
+
         """
-        abspath = self.abspath(project_dir)
-        relpath = os.path.relpath(abspath, os.getcwd())
+        relpath = os.path.relpath(self.abspath())
         return relpath
+
+    def abspaths(self):
+        """Paths of available assets
+
+        Get absolute paths of asset files in first level of asset directory
+
+        Returns:
+            tuple: file paths
+
+        """
+        file_pattern = os.path.join(self.abspath(), '*.'+self.file_extension)
+        files = glob.glob(file_pattern)
+        return tuple(files)
+
+    def files(self):
+        """Available asset files
+
+        Get asset filenames in first level of asset directory
+
+        Returns:
+            tuple: asset filenames
+        """
+        files = [os.path.basename(filepath) for filepath in self.abspaths()]
+        return tuple(files)
+
+    def print_files(self):
+        """Print asset files, newline delimitedfile names
+        """
+        [print(asset_file) for asset_file in self.files()]
