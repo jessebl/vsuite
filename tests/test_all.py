673b163e078ceda02b4dc42a412b8ce9c288978c
Jesse Bulson-Lewis <bulsonjg@whitman.edu>
2018-02-19 14:14:07 -0800

    Unify test suite

---
+++ tests/test_all.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+
+def suite():
+    loader = unittest.TestLoader()
+    start_dir = os.getcwd()
+    suite = loader.discover(start_dir)
+    return suite
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
